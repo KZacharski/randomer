b295e1169d98af4bf46c3c33d2200cd72bbabde3
Kacper Zacharski <kacper@zacharski.hub.pl>
2020-12-10 23:53:28 +0100

    some changes

---
+++ config.js
@@ -0,0 +1,4 @@
+var configjson = '{ "edited" : false, "editorName" : "Your name.", "min" : 0, "max" : 1000}'
+var obj = JSON.parse(configjson);
+cssVar('bgcolor', '#ffffff');
+cssVar('txtcolor', '#33a000');
